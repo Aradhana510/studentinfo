c13edf47ecec7dfc30430d78234c92b27d2d647a
Aradhana singh <66427825+Aradhana510@users.noreply.github.com>
2023-07-25 02:52:48 -0700

    Add files via upload

---
+++ studentsinfo/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+
+namespace StudentDataConsoleApp
+{
+    class Program
+    {
+        private const string Value = "ID\tFirstName \t LastName \t Grade";
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                // Specify the file path where the student data is stored
+                string filePath = "students.txt";
+
+                // Read all lines from the text file
+                string[] lines = File.ReadAllLines(filePath);
+
+                // Display the data on the console
+                Console.WriteLine("Student Data:");
+
+                foreach (string line in lines)
+                {
+                    string[] data = line.Split(',');
+                    string studentID = data[0];
+                    string firstName = data[1];
+                    string lastName = data[2];
+                    string grade = data[3];
+
+                    Console.WriteLine($"{studentID}\t{firstName}\t{lastName}\t{grade}");
+                }
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("Error: File not found.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
